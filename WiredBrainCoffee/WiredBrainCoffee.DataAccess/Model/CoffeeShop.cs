e58881469fcae3a9be4f7019964e2419d0ef4c40
juanrangeljuarez <juanrangel1@mail.weber.edu>
2019-08-26 16:39:13 -0600

    help command

---
+++ WiredBrainCoffee/WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WiredBrainCoffee.DataAccess.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+        public int BeansInStockInKg { get; set; }
+
+    }
+}
